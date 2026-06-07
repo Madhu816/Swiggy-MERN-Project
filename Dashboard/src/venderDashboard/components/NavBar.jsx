7f1656cd9e728dd01f6deb4196eb2ad732b60225
Madhu816 <madhupiske816@gmail.com>
2026-06-07 19:46:54 +0530

    Fix dashboard URL

--- Dashboard/src/venderDashboard/components/NavBar.jsx
+++ Dashboard/src/venderDashboard/components/NavBar.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 //pasing props
-const NavBar = ({OnLoginClick,OnRegister,showLogout,logoutHandler}) => {
-    const firmName=localStorage.getItem('firmname');
+const NavBar = ({ OnLoginClick, OnRegister, showLogout, logoutHandler }) => {
+    const firmName = localStorage.getItem('firmname');
     console.log(firmName);
     return (
         <div className="navSection">
-            <a href="https://swiggy-client-dashboard.vercel.app/" target="_blank"  style={{textDecoration:"none",color:"white"}}className="company"> Vender-DashBoard</a>
-            {/* <div className="company">Vender-DashBoard</div> */}
+            <a
+                href="https://swiggy-dashboard-black.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "white" }}
+                className="company"
+            >
+                Vender-DashBoard
+            </a>            {/* <div className="company">Vender-DashBoard</div> */}
             <div className="firmname">
-                {firmName? <h4>FirmName : {firmName} Restarent</h4>:"Home-Page"}
+                {firmName ? <h4>FirmName : {firmName} Restarent</h4> : "Home-Page"}
             </div>
-                <div className="userAuth">
-                    {!showLogout ?<>
+            <div className="userAuth">
+                {!showLogout ? <>
                     <span onClick={OnLoginClick}>Login/</span>
                     <span onClick={OnRegister}>Register</span>
-                    </> :<span onClick={logoutHandler}>Logout</span>
-                    }
+                </> : <span onClick={logoutHandler}>Logout</span>
+                }
 
-                </div>
+            </div>
         </div>
     )
 }
